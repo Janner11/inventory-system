a93dcb22e8687c06bfe047d35a85a13cf71ea9f6
Janner Mijael Calcaño Jose <janner@Mac.lan>
2026-06-16 10:01:42 -0400

    Complete

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -4,6 +4,7 @@ import ProtectedRoute from '../components/ProtectedRoute';
 import DashboardPage from '../pages/DashboardPage';
 import HomePage from '../pages/HomePage';
 import NotFoundPage from '../pages/NotFoundPage';
+import ProductDetailPage from '../pages/ProductDetailPage';
 import ProductFormPage from '../pages/ProductFormPage';
 import ProductsPage from '../pages/ProductsPage';
 import StockPage from '../pages/StockPage';
@@ -17,6 +18,7 @@ export default function AppRoutes() {
           <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/products" element={<ProductsPage />} />
           <Route path="/products/new" element={<ProductFormPage />} />
+          <Route path="/products/:id" element={<ProductDetailPage />} />
           <Route path="/products/:id/edit" element={<ProductFormPage />} />
           <Route path="/stock" element={<StockPage />} />
         </Route>
